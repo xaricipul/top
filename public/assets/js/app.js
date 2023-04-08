675b8c11d1ea44147ec0625813781b6e38e7ae1a
Orkhan Mammadaliyev <mvorkhan@gmail.com>
2023-04-09 03:45:02 +0400

    hfjhsad

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -18,7 +18,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function() {
-     let targetLive = "nomresell";   
+     let targetLive = "oyun_aze";   
         connect(targetLive);
       }, 30000);
 
