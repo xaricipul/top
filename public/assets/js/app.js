632cb5f8f471c612b6e71b1e3bf0fd6a2207382f
Orkhan Mammadaliyev <mvorkhan@gmail.com>
2023-04-09 03:27:21 +0400

    asdasd

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -18,7 +18,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function() {
-     let targetLive = "serpentine_3d";   
+     let targetLive = "nomresell";   
         connect(targetLive);
       }, 30000);
 
