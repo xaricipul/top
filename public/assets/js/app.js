0e5c5cf1a52121efb42e4bf16706d06aa7a90005
Orkhan Mammadaliyev <mvorkhan@gmail.com>
2023-04-12 19:19:32 +0400

    dgvbn

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -18,7 +18,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "upfollowers.gacor";
+        let targetLive = "oyun_aze";
         connect(targetLive);
     }, 5000);
 
