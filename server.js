3f4d5c84e18d56a1a3ac1a9ff41e64b64abdedb7
Orkhan Mammadaliyev <mvorkhan@gmail.com>
2023-04-13 20:24:05 +0400

    fdgfff

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ setInterval(() => {
 app.use(express.static('public'));
 
 // Start http listener
-const port = process.env.PORT || 8019;
+const port = process.env.PORT || 8020;
 httpServer.listen(port);
 console.info(`Server running! Please visit http://localhost:${port}`);
