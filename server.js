7ca347c3cb6ba19417819ce309735777283494f3
Orkhan Mammadaliyev <mvorkhan@gmail.com>
2023-04-15 03:35:04 +0400

    sads

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ setInterval(() => {
 app.use(express.static('public'));
 
 // Start http listener
-const port = process.env.PORT || 8026;
+const port = process.env.PORT || 8027;
 httpServer.listen(port);
 console.info(`Server running! Please visit http://localhost:${port}`);
