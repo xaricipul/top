64522b4db3c8e3e6dd2c9e46782746ca33ef2b31
mvorxan <117027967+mvorxan@users.noreply.github.com>
2023-05-30 16:20:33 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -18,7 +18,7 @@ let messagesQueue = [];
 $(document).ready(() => {
 
     setTimeout(function () {
-        let targetLive = "@game_live_az";
+        let targetLive = "@oyun+aze";
         connect(targetLive);
     }, 5000);
 })
