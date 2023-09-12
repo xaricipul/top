2a94510ccff36116c78b1c68ecb142b2b660adb5
mvorxan <117027967+mvorxan@users.noreply.github.com>
2023-09-13 02:31:12 +0400

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ app.use(express.static('public'));
 
 
 // Start http listener
-const port = process.env.PORT || 8022;
+const port = process.env.PORT || 8023;
 httpServer.listen(port);
 console.info(`Server running! Please visit http://localhost:${port}`);
