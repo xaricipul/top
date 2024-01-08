6596d68951722ce63a716bfaeca2e80caf087f79
testliveaz <155069408+testliveaz@users.noreply.github.com>
2024-01-09 01:30:55 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "mirtlive";
+        let targetLive = "wallviral";
         connect(targetLive);
     }, 5000);
 
