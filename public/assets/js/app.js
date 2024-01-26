8fee423d008c7dd23047ff47eb4cfb8fc58b4119
testliveaz <155069408+testliveaz@users.noreply.github.com>
2024-01-27 02:32:48 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "mirtlive1";
+        let targetLive = "clickaz";
         connect(targetLive);
     }, 5000);
 
