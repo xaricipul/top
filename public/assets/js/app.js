a9081dfc43379f6747bafa13b1b6d86f0f21113f
testliveaz <155069408+testliveaz@users.noreply.github.com>
2024-01-27 01:48:54 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "azbesdir";
+        let targetLive = "12bughf1";
         connect(targetLive);
     }, 5000);
 
