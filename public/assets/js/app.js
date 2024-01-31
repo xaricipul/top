35ee8861abae92b6b2752f11c1546880684118a4
testliveaz <155069408+testliveaz@users.noreply.github.com>
2024-01-31 21:22:47 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "mirtlive1";
+        let targetLive = "gamefhon";
         connect(targetLive);
     }, 5000);
 
