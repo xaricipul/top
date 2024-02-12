82b3fd02bbde078ffe0bbfaccfdda1c30f25a5a2
testliveaz <155069408+testliveaz@users.noreply.github.com>
2024-02-12 13:00:19 +0400

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ app.use(express.static('public'));
 
 
 // Start http listener
-const port = process.env.PORT || 3520;
+const port = process.env.PORT || 5555;
 httpServer.listen(port);
 console.info(`Server running! Please visit http://localhost:${port}`);
