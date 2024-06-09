ae89be36c756369aff7395222af39468ea2e2fdc
xaricipul <xaricipul@gmail.com>
2024-06-10 02:09:44 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "fasdungel";
+        let targetLive = "kaseke.37";
         connect(targetLive);
     }, 5000);
 
