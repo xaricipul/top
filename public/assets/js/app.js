59efc62f019fd0bc20408d04a6d9638467721794
xaricipul <xaricipul@gmail.com>
2024-07-22 04:16:14 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
     // });
 
     setTimeout(function () {
-        let targetLive = "h3midovaa";
+        let targetLive = "azbesdir";
         connect(targetLive);
     }, 5000);
 
